24eeebcbfcc50b475fa963637ff51c1b77394482
wmvieira21 <williammartinsvieira334@gmail.com>
2025-01-17 13:03:18 -0300

    bottom sheet modal created

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,6 @@
+import 'package:expenses_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -11,10 +13,40 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpense extends State<NewExpense> {
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  String _selectedDate = 'No date selected';
+  Category _selectedCategory = Category.food;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(
+      now.year - 1,
+      now.month,
+      now.day,
+    );
+    final pickedDate = await showDatePicker(
+        context: context, firstDate: firstDate, lastDate: now);
+
+    if (pickedDate != null) {
+      setState(() {
+        _selectedDate = formatter.format(pickedDate);
+      });
+    }
+  }
+
+  void onChangeCategory(Category? selectedItem) {
+    if (selectedItem == null) {
+      return;
+    }
+    setState(() {
+      _selectedCategory = selectedItem;
+    });
+  }
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -23,6 +55,7 @@ class _NewExpense extends State<NewExpense> {
     return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
+        spacing: 15,
         children: [
           TextField(
             controller: _titleController,
@@ -34,8 +67,52 @@ class _NewExpense extends State<NewExpense> {
           ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: '\$ ',
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(_selectedDate),
+                    IconButton(
+                        onPressed: () => _presentDatePicker(),
+                        icon: Icon(Icons.calendar_month)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            spacing: 10,
+            children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(category.name.toUpperCase()),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (selectedItem) => onChangeCategory(selectedItem)),
+              const Spacer(),
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text('Cancel'),
+              ),
               ElevatedButton(
-                onPressed: (() => print(_titleController.text)),
+                onPressed: (() =>
+                    print(_titleController.text + _amountController.text)),
                 child: Text('Save expense'),
               ),
             ],
