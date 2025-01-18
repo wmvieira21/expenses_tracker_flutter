37a6c8ce4f3223d695e60e4a46736b6caf7298bf
wmvieira21 <williammartinsvieira334@gmail.com>
2025-01-18 16:37:08 -0300

    begining with theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
 import 'package:expenses_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorsScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorsScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorsScheme.onPrimaryContainer,
+          foregroundColor: kColorsScheme.onPrimary,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorsScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorsScheme.primaryContainer),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: kColorsScheme.onSecondaryContainer,
+                  fontSize: 18),
+            ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
