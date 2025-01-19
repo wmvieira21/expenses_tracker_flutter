68ccc56d1e7845aea3ed225cdaf7477eb980dec6
wmvieira21 <williammartinsvieira334@gmail.com>
2025-01-19 13:53:30 -0300

    setPreferredOrientations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorsScheme =
     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
@@ -8,6 +9,9 @@ var kColorsSchemeDark = ColorScheme.fromSeed(
     seedColor: Color.fromARGB(255, 5, 99, 125), brightness: Brightness.dark);
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //     .then((fn) {
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
@@ -46,4 +50,5 @@ void main() {
       home: const Expenses(),
     ),
   );
+  // });
 }
